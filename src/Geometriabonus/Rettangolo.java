2acd8d6365c4e5fbfd4a42ba34ea87388278db66
EspoTony <espotony98@gmail.com>
2022-06-14 15:23:56 +0200

    bonus finito

---
+++ src/Geometriabonus/Rettangolo.java
@@ -0,0 +1,18 @@
+package Geometriabonus;
+
+public class Rettangolo {
+
+	 int base;
+	    int altezza;
+
+	     int area() {
+	        int area = base*altezza;
+	        return area;
+	    }
+
+	    int perimetro() {
+	         int perimetro = (base+altezza)*2;
+	         return perimetro;
+	    }
+
+}
